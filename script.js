46b2207e0bc0f34feba64be135c74ab0b80fe7ae
Maekin <74455990+mekintown@users.noreply.github.com>
2023-06-21 11:55:45 +0700

    Add Book Constructor

---
+++ script.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function Book(author, title, pagesNumber, isRead) {
+    this.author = arthor;
+    this.tile = title;
+    this.pagesNumber = pagesNumber;
+    this.isRead = isRead;
+}
+
+function addBookToLibrary() {
+
+}
